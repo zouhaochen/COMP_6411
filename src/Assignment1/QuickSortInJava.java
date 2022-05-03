fa112bebb934ea1d428efd42eb0192284b2d8e2f
NormalZHC <zouhaochenluke@gmail.com>
2022-05-03 12:25:31 -0400

    quick sort

--- src/Assignment1/QuickSortInJava.java
+++ src/Assignment1/QuickSortInJava.java
@@ -1,18 +1,63 @@
 package Assignment1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class QuickSortInJava
 {
+    private static String randomNumberPath = "src/Assignment1/rand.txt";
+
     public static void main(String[] args)
     {
-
+        File randomNumber = new File(randomNumberPath);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList = txtToArrayList(randomNumber);
+        QuickSort(arrayList, 0, arrayList.size() - 1);
     }
+
+    /**
+     * Transfer txt file into array list
+     * @param file file contain random number
+     * @return array list with file number
+     */
+    public static ArrayList txtToArrayList(File file)
+    {
+        ArrayList<Integer> arrayList = new ArrayList();
+        try
+        {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String s = null;
+            while((s = bufferedReader.readLine())!=null)
+            {
+                int number = 0;
+                try
+                {
+                    number = Integer.parseInt(s);
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Fail to parse string into integer");
+                }
+                arrayList.add(number);
+            }
+            bufferedReader.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+
     /**
      * Sort the array from small to large
-     * @param array the content of the array to sort
-     * @param p the start index of the array
-     * @param r the end index of the array
+     * @param array the content of the array to sort list
+     * @param p the start index of the array list
+     * @param r the end index of the array list
      */
-    public static void QuickSort(int[] array, int p, int r)
+    public static void QuickSort(ArrayList<Integer> array, int p, int r)
     {
         if(p < r)
         {
@@ -24,28 +69,28 @@ public class QuickSortInJava
 
     /**
      * Partition the array into 2 subarray around pivot
-     * @param array the content of the array
-     * @param p the start index of the array
-     * @param r the end index of the array
+     * @param array the content of the array list
+     * @param p the start index of the array list
+     * @param r the end index of the array list
      * @return
      */
-    public static int Partition(int[] array, int p, int r)
+    public static int Partition(ArrayList<Integer> array, int p, int r)
     {
-        int x = array[r];
+        int x = array.get(r);
         int i = p - 1;
         for (int j = p; j < r; j ++)
         {
-            if(array[j] <= x)
+            if(array.get(j) <= x)
             {
                 i = i + 1;
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                int temp = array.get(i);
+                array.set(i, array.get(j));
+                array.set(j, temp);
             }
         }
-        int exchange = array[r];
-        array[r] = array[i + 1];
-        array[i + 1] = exchange;
+        int exchange = array.get(r);
+        array.set(r, array.get(i + 1));
+        array.set(i + 1, exchange);
         return i + 1;
     }
 }
