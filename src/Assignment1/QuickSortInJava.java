63bc92832e22556e35e92d9ceddf77b70d79aa05
NormalZHC <zouhaochenluke@gmail.com>
2022-05-05 09:45:40 -0400

    quick sort

--- src/Assignment1/QuickSortInJava.java
+++ src/Assignment1/QuickSortInJava.java
@@ -1,24 +1,64 @@
 package Assignment1;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 
 public class QuickSortInJava
 {
+    //input and output file path
     private static String randomNumberPath = "src/Assignment1/rand.txt";
+    private static String outputPath = "src/Assignment1/output.txt";
 
     public static void main(String[] args)
     {
         File randomNumber = new File(randomNumberPath);
+        System.out.println("The random number file read.");
+
+        //use array list to store the random number information
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList = txtToArrayList(randomNumber);
         QuickSort(arrayList, 0, arrayList.size() - 1);
+
+        //output result in console
         for(int i = 0; i < arrayList.size(); i++)
         {
             System.out.println(arrayList.get(i));
         }
+
+        //write result in the output file
+        BufferedWriter bufferedWriter = null;
+        try
+        {
+            FileWriter fileWriter = new FileWriter(outputPath);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            for(int i = 0; i < arrayList.size(); i++)
+            {
+                bufferedWriter.write(String.valueOf(arrayList.get(i)));
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+            bufferedWriter.close();
+        }
+        catch(IOException e)
+        {
+            throw new RuntimeException("Failed to write file.");
+        }
+        finally
+        {
+            try
+            {
+                if(bufferedWriter != null)
+                {
+                    bufferedWriter.close();
+                }
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException("Failed to close file.");
+            }
+        }
+
+        System.out.println("The result output file create.");
     }
 
     /**
@@ -82,6 +122,7 @@ public class QuickSortInJava
     {
         int x = array.get(r);
         int i = p - 1;
+
         for (int j = p; j < r; j ++)
         {
             if(array.get(j) <= x)
@@ -92,6 +133,7 @@ public class QuickSortInJava
                 array.set(j, temp);
             }
         }
+
         int exchange = array.get(r);
         array.set(r, array.get(i + 1));
         array.set(i + 1, exchange);
